79b8ee3acd1bc8045ad4cdb08c4bab17d6475a0b
Leonardo Gomez <lfglfg0804@gmail.com>
2025-08-22 09:38:54 -0300

    Creo Repositorio y agrego Actividad 1 (ToString)

---
+++ Actividad1/src/Estudiante.java
@@ -0,0 +1,20 @@
+public class Estudiante extends Persona{
+    private String carrera;
+
+    public Estudiante(String nombre,int edad,String carrera){
+        super(nombre,edad);
+        this.carrera=carrera;
+    }
+    public String getCarrera() {
+        return carrera;
+    }
+    public void setCarrera(String carrera) {
+        this.carrera = carrera;
+    }
+   @Override
+    public String toString(){
+
+        return super.toString().replace("}",", ").replace("Persona","Estudiante")
+                + "carrera=" + carrera + '}';
+   }
+}
